b3cdc0aa60a59cf958fc0b675f1cf8a5eb12045f
vnzoliveira <tploxvrdct@protonmail.ch>
2024-03-30 00:46:03 -0300

    Organizando codigo seminario

---
+++ java/DispositivoIoT.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+class DispositivoIoT{
+    private String id;
+    private double temperatura;
+
+    // Construtor da classe DispositivoIoT
+    public DispositivoIoT(String id) {
+        this.id = id;
+        this.temperatura = 0.0; // Define a temperatura inicial como 0°C
+    }
+
+    // Método para gerar uma temperatura aleatória
+    public void gerarTemperatura() {
+        Random random = new Random();
+        this.temperatura = 20 + (random.nextDouble() * 15); // Definidindo uma temperatura aleatória entre 20°C e 35°C
+    }
+
+    // Método para enviar os dados de temperatura para o servidor ficticio
+    public void enviarDadosParaServidor() {
+        String temperaturaFormatada = String.format("%.2f", temperatura);
+        System.out.println(
+                "Enviando dados do dispositivo " + id + " para o servidor: Temperatura = " + temperaturaFormatada + "°C");
+                // Código para enviar os dados para o servidor
+                // Utilizando print para demonstracao do programa
+    }
+
+
+}
